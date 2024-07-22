dd47ef4c151da2e14ae805de2651d6d5dcde5cab
Carla <carla@MacBook-Air-de-Carla.local>
2024-07-22 11:39:11 -0300

    Adicionando teste de Login

---
+++ README.md
@@ -0,0 +1,20 @@
+# Seu Barriga
+## Automação do Login
+
+Como executar o teste:
+1- Criar uma pasta na sua máquina com o nome "seubarriga-teste"
+
+2- No vs code,
+- Abrir a pasta "seubarriga-teste"
+3- Abrir o terminal
+- clonar o repositório do git - git clone https://github.com/CahKitty/seubarriga-automation.git
+- instalar o cypress com o comando - "npm install cypress"
+- Rodar o cypress como o comando "npx cypress open"
+
+4- Quando o sistema carregar a tela do cypress, selecione o "Electron", e em seguida, a spec "Login". A automação será iniciada 👍🏼
+
+Dúvida / Suporte
+carla.annunciato@gmail.com
+
+
+
---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,19 @@
+describe('Login', () => {
+  beforeEach('', () => {
+    cy.visit('https://seubarriga.wcaquino.me/login')
+  })
+
+  it('sucesso ao logar na plataforma', () => {
+    cy.get('#email').type('CARLA.ANNUNCIATO@GMAIL.COM')
+    cy.get('#senha').type('discovoador123')
+    cy.get('.btn').click()
+    cy.contains('Bem vindo, Carla Annunciato!').should('be.visible')
+  })
+
+  it('erro ao logar na plataforma', () => {
+    cy.get('#email').type('CARLA.ANNUNCIATO@GMAIL.COM')
+    cy.get('#senha').type('outrogato345')
+    cy.get('.btn').click()
+    cy.contains('Problemas com o login do usuário').should('be.visible')
+  })
+})
